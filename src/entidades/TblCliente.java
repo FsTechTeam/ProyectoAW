43bf67f63aba1edd336a9015c5a0a74cc684bc66
frankzapeta <frank.zapeta@hotmail.com>
2017-01-18 20:58:20 -0600

    Delete lines on table tblCliente (Generate value, basic)

--- src/entidades/TblCliente.java
+++ src/entidades/TblCliente.java
@@ -45,8 +45,6 @@ import javax.xml.bind.annotation.XmlTransient;
 public class TblCliente implements Serializable {
     private static final long serialVersionUID = 1L;
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Basic(optional = false)
     @Column(name = "id")
     private Integer id;
     @Column(name = "nombre")
